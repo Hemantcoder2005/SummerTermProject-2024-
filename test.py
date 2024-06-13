cbac7f018f980759d243a71765f4cac59ee06ca6
Hemantcoder2005 <hemantnarula2203@outlook.com>
2024-06-13 15:23:50 +0530

    testSensors

---
+++ test.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+for pin in MOTOR_PINS.values():
+    GPIO.setup(pin, GPIO.OUT)
+
+SENSOR_PINS = {
+    'central': {'trigger': 16, 'echo': 18}
+}
+
+for sensor in SENSOR_PINS.values():
+    GPIO.setup(sensor['trigger'], GPIO.OUT)
+    GPIO.setup(sensor['echo'], GPIO.IN)
+
+
+def measure_distance(trigger, echo):
+    GPIO.output(trigger, False)
+    time.sleep(0.2)
+    GPIO.output(trigger, True)
+    time.sleep(0.00001)
+    GPIO.output(trigger, False)
+    start, stop = time.time(), time.time()
+    while GPIO.input(echo) == 0:
+        start = time.time()
+    while GPIO.input(echo) == 1:
+        stop = time.time()
+    elapsed = stop - start
+    distance = elapsed * 34000 / 2
+    print(distance)
+    return distance
+
+while True:
+    measure_distance(16,18)
